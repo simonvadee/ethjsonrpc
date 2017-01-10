2bf3636923f9d41abcf50bb8588409136a2ca90f
Simon Vadée <simon.vadee@gmail.com>
2017-01-10 17:43:40 +0100

    update call_with_transaction to remove bytecode

--- ethjsonrpc/client.py
+++ ethjsonrpc/client.py
@@ -128,7 +128,7 @@ class EthJsonRpc(object):
         gas = gas or self.DEFAULT_GAS_PER_TX
         gas_price = gas_price or self.DEFAULT_GAS_PRICE
         data = self._encode_function(sig, args)
-        data_hex = encode_hex(data)
+        data_hex = '0x' + encode_hex(data).decode('utf-8')
         return self.eth_sendTransaction(from_address=from_, to_address=address, data=data_hex, gas=gas,
                                         gas_price=gas_price, value=value)
 
